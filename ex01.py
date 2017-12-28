d02adb0dce7bce2e32732aebb09a6b68e10db83f
Zayd Ghoggali <zaydelec@gmail.com>
2017-12-28 19:59:08 +0100

    Add ex1-3 to repo.

---
+++ ex01.py
@@ -0,0 +1,9 @@
+# This is the first exercise of Zed Shaw's book
+
+print("Hello World")
+print("Hello again")
+print("I like typing this.")
+print("This is fun")
+print("Yay! Printing")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
---
+++ ex03.py
@@ -0,0 +1,22 @@
+print("I will now count my chickens: ")
+print("Hens", 25 + 30 / 6)
+
+print("Roosters", 100 - 25 * 3 % 4)
+print("Now, I will count the eggs: ")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6)
+print("Is it true that 3 + 2 < 5 - 7 ?")
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh! That's why it's false")
+print("How about some more?")
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5<= -2)
+
+print
+print("These show math with integers vs floating point numbers")
+print("5/4 = ", 5/4, "The .25 got truncated")
+print("5.0/4 = ", 5.0 / 4)
+
