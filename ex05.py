4c668dce1a266fdf0f1c2e8c7b1f6984256fad6a
Zayd Ghoggali <zaydelec@gmail.com>
2017-12-29 09:48:52 +0100

    Add ex04 and ex05

---
+++ ex05.py
@@ -0,0 +1,16 @@
+my_name = "Zayd Gho"
+my_age = 35 # not a lie
+my_height = 74 # in inches
+my_weight = 180 # in lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "Let's talk about %s." % my_name
+print "He's %s inches tall." % my_height
+print "He's %s pounds heavy." % my_weight
+print "Actually, that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+print "If I add %d, %d and %d, I get %d." % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
