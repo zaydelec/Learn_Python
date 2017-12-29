471a2879dcb5fdb7bdab26c5978c4377d38da4e0
Zayd Ghoggali <zaydelec@gmail.com>
2017-12-29 15:26:57 +0100

    Add ex10-11

---
+++ ex10.py
@@ -0,0 +1,20 @@
+# This exercise is about inserting escape sequences in Python
+
+tabby_cat = "\tI'm tabbed in."
+# \t inserts a tab
+
+persian_cat = "I'm split\non a line."
+# \n goes to new line.
+
+backslash_cat = "I'm \\ a \\ cat."
+# \\ prints a single \
+
+fat_cat = ''' I'll do a list:
+\t* Cat food.
+\t* Fishies.
+\t* Catnip.\n\t* Grass.'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ ex11.py
@@ -0,0 +1,17 @@
+print "How old are you?",
+age = raw_input()
+
+print "How tall are you?",
+height = raw_input()
+
+print "How much do you weigh?",
+weight = raw_input()
+
+print
+print "So, you're %r old, %r tall, and %r heavy." % (
+	age, height, weight)
+
+print 
+print ("So, you're %r years old, %r inches tall, \n"
+	"	and %r pounds heavy.") % (age, height, weight)
+	
