a3ea6bdd9de1ca78a1d4df585b79594341930d26
Zayd Ghoggali <zaydelec@gmail.com>
2017-12-30 20:10:05 +0100

    Addex16-18

---
+++ ex16_text.txt
@@ -0,0 +1,3 @@
+This is the 1st line
+This is the 2nd line
+This is the 3rd line
---
+++ ex18.py
@@ -0,0 +1,21 @@
+def print_two(*args):
+	arg1, arg2 = args
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+	
+
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+	
+	
+def print_one(arg1):
+	print "arg1: %r" % arg1
+	
+	
+def print_none():
+	print "I got nothing."
+	
+
+print_two('Zayd', 'Ghoggali')
+print_two_again('Zayd', 'Ghoggali')
+print_one("First!")
+print_none()
